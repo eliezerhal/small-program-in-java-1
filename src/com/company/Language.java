8c59840eaa9d598c84194fe304b89c4b840b26e2
אלי הלטובסקי <eliezerhal@edu.hac.ac.il>
2021-04-08 18:22:15 +0300

    8.4.21

--- src/com/company/Language.java
+++ src/com/company/Language.java
@@ -2,36 +2,43 @@ package com.company;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 
 public class Language implements command {
     public Language(String[] str) {
         myStr = str;
     }
     public boolean checkingContentType() {
-        if(isEnglish())
-            return true;
-        return false;
+        return isEnglish();
     }
 
     public boolean isEnglish() {
-        int[] letteresArr = new int[27];
+        int[] lettersArr = new int[26];
         try {
             String str = readText();
+            int counter = 0;
             for (int i = 0; i < str.length(); i++) {
-                if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
-                    letteresArr[str.charAt(i) - 'a']++;
+                if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                    lettersArr[str.charAt(i) - 'a']++;
+                    counter++;
                 }
-        }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (Exception e) {
+            }
+            double[] frqArr = new double[26];
+            double[] FrqArr = {0.0748, 0.0134, 0.0411, 0.0308, 0.1282, 0.0240,
+                    0.0185, 0.0414, 0.0725, 0.0014, 0.0053, 0.0403, 0.0340,
+                    0.0673, 0.0785, 0.0314, 0.0010, 0.0609, 0.0614, 0.1002,
+                    0.0316, 0.0108, 0.0131, 0.0044, 0.0127, 0.0011};
+            for (int i = 0; i < 26; i++) {
+                frqArr[i] = (double) lettersArr[i]/counter;
+            }
+            for (int i = 0; i < 26; i++) {
+                double diff = FrqArr[i] - frqArr[i];
+                if(diff * diff > 0.04)
+                    return false;
+            }
+            return true;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return true;
@@ -42,5 +49,5 @@ public class Language implements command {
         str = str.toLowerCase();
         return str;
     }
-    private String[] myStr;
+    private final String[] myStr;
 }
